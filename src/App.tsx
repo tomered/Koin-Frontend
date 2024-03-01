21fe0a33d12f0a9bd7e439392a4235d379848c55
Tomer Edelsberg <tomeredel@gmail.com>
2024-03-01 12:09:28 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import NavBarTextItem from "./Components/NavBarTextItem/NavBarTextItem";
+import HomeScreenPage from "./pages/HomeScreenPage";
+
+function App() {
+  return (
+    <>
+    
+      <HomeScreenPage />
+    </>
+  );
+}
+
+export default App;
